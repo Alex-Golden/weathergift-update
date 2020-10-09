14125fb9dd0c5627958ca43beff26b685704db75
Alex Golden <goldenah@bc.edu>
2020-10-08 22:39:43 -0400

    5.6

---
+++ WeatherGift Demo/LocationDetailViewController.swift
@@ -0,0 +1,52 @@
+//
+//  LocationDetailViewController.swift
+//  WeatherGift Demo
+//
+//  Created by Alex Golden on 10/8/20.
+//
+
+import UIKit
+
+class LocationDetailViewController: UIViewController {
+    @IBOutlet weak var dateLabel: UILabel!
+    @IBOutlet weak var placeLabel: UILabel!
+    @IBOutlet weak var temperatureLabel: UILabel!
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var summaryLabel: UILabel!
+    
+    var weatherLocation: WeatherLocation!
+    var weatherLocations: [WeatherLocation] = []
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        if weatherLocation == nil {
+            weatherLocation = WeatherLocation(name: "current location", latitude: 0.0, longitude: 0.0)
+            weatherLocations.append(weatherLocation)
+        } else {
+            
+        }
+        
+        
+updateUserInterface()
+    
+    }
+    func updateUserInterface() {
+        dateLabel.text = ""
+        placeLabel.text = weatherLocation.name
+        temperatureLabel.text = "--°"
+        summaryLabel.text = ""
+    }
+
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let destination = segue.destination as! LocationListViewController
+        destination.weatherLocations = weatherLocations
+    }
+    
+    @IBAction func unwindFromLocationListViewController(segue: UIStoryboardSegue){
+        let source = segue.source as! LocationListViewController
+        weatherLocations = source.weatherLocations
+        weatherLocation = weatherLocations[source.selectedLocationIndex]
+        updateUserInterface()
+    }
+}
